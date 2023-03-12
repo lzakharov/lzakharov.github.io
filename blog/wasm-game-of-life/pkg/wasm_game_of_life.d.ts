973d3d1827e4935ea3a5677652ccf72143d8005c
lzakharov <lzakharov@users.noreply.github.com>
2023-03-12 17:04:09 +0000

    deploy: 941ea19c0f3dbdeae68c7d6d4d21949658f12af5

---
+++ blog/wasm-game-of-life/pkg/wasm_game_of_life.d.ts
@@ -0,0 +1,59 @@
+/* tslint:disable */
+/* eslint-disable */
+/**
+*/
+export class Universe {
+  free(): void;
+/**
+* @returns {Universe}
+*/
+  static new(): Universe;
+/**
+*/
+  tick(): void;
+/**
+* @returns {number}
+*/
+  width(): number;
+/**
+* @returns {number}
+*/
+  height(): number;
+/**
+* @returns {number}
+*/
+  cells(): number;
+}
+
+export type InitInput = RequestInfo | URL | Response | BufferSource | WebAssembly.Module;
+
+export interface InitOutput {
+  readonly memory: WebAssembly.Memory;
+  readonly __wbg_universe_free: (a: number) => void;
+  readonly universe_new: () => number;
+  readonly universe_tick: (a: number) => void;
+  readonly universe_width: (a: number) => number;
+  readonly universe_height: (a: number) => number;
+  readonly universe_cells: (a: number) => number;
+}
+
+export type SyncInitInput = BufferSource | WebAssembly.Module;
+/**
+* Instantiates the given `module`, which can either be bytes or
+* a precompiled `WebAssembly.Module`.
+*
+* @param {SyncInitInput} module
+*
+* @returns {InitOutput}
+*/
+export function initSync(module: SyncInitInput): InitOutput;
+
+/**
+* If `module_or_path` is {RequestInfo} or {URL}, makes a request and
+* for everything else, calls `WebAssembly.instantiate` directly.
+*
+* @param {InitInput | Promise<InitInput>} module_or_path
+*
+* @returns {Promise<InitOutput>}
+*/
+export default function init (module_or_path?: InitInput | Promise<InitInput>): Promise<InitOutput>;
